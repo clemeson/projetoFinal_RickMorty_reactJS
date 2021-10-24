3cfa1084efbc596866f23f392097885d438188a2
unknown <clemeson4lves@gmail.com>
2021-10-24 15:00:32 -0300

    configurando express

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
-console.log('subido')
+ 
+ //importando framwork express
+ const express = require('express');
+ //pegando resolve do modulo path do node.
+ const { resolve } = require('path')
+ const app = express();
+
+ //criando midlleware para observar o diretorio raiz, servir a pasta build.
+ app.use('/', express.static(resolve(__dirname, './build')))
+
+ app.listen(process.env.PORT || 3000, (err)=>{
+    if(err){
+        return console.log(err)
+    }
+
+    console.log('tudo ok')
+ }  
