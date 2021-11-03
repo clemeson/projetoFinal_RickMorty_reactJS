a0fa2e383c59daa45ef663a89b29d978694c291a
unknown <clemeson4lves@gmail.com>
2021-11-03 15:26:55 -0300

    -nao sei oq houve

--- src/components/header/HeaderComponent.js
+++ src/components/header/HeaderComponent.js
@@ -3,8 +3,79 @@ import React from "react";
 import {Link} from 'react-router-dom'
 import './header.css'
 import Btn from "./btn/Btn";
-import Logo from './img/logo_rick.png'
-import Icon from './img/icon-menu.svg'
+import Logo from './img/logo_rick.png';
+import Icon from './img/icon-menu.svg';
+import styled, {keyframes} from 'styled-components';
+
+
+const keyLine = keyframes`
+0%{
+    border-width: 2px solid #74E452;
+    width: 1%;
+    transition: all 0.5s ease-out
+}
+
+50%{
+    border: 2px solid #74E452;
+    width: 50%;
+    box-shadow: 0px 0px 35px #74E452, 0px 0px 15px #74E452 inset;
+
+}
+100%{
+    border: 2px solid #74E452;
+    width: 90%;
+    box-shadow: 0px 0px 35px #74E452, 0px 0px 15px #74E452 inset;
+}
+
+`
+
+
+const keyLine2 = keyframes`
+0%{
+    border-width: 2px solid #D705F2;
+    width: 1%;
+    transition: all 0.5s ease-out
+}
+
+50%{
+    border: 2px solid #D705F2;
+    width: 50%;
+    box-shadow: 0px 0px 35px #D705F2, 0px 0px 15px #D705F2 inset;
+
+}
+100%{
+    border: 2px solid #D705F2;
+    width: 90%;
+    box-shadow: 0px 0px 35px #D705F2, 0px 0px 15px #D705F2 inset;
+}
+
+
+`
+const Hr = styled.hr`
+    
+    animation-name: ${keyLine};
+    animation-duration: 1.2s;
+    animation-timing-function: ease;
+    border: 2px solid #74E452;
+
+
+    box-shadow: 0px 0px 35px #74E452, 0px 0px 15px #74E452 inset;
+
+    
+    
+
+`
+
+
+const HrPurple = styled.hr`
+
+animation-name: ${keyLine2};
+animation-duration: 1.2s;
+border: 2px solid #D705F2;
+
+    box-shadow: 0px 0px 35px #D705F2, 0px 0px 15px #D705F2 inset;
+
+`
 
 
 export default function Header() {
@@ -15,6 +86,7 @@ export default function Header() {
     }
 
     return(
+        <>
         <header id='header'>
             <img id='logo' src={Logo}></img>
             <nav id='nav'>
@@ -26,19 +98,22 @@ export default function Header() {
 
                 <ul id='menu'>
                     <li>
-                    <Link to="/">Home</Link>
+                    <Link className="link" to="/"><Btn text='Home'/></Link>
                     </li>
                     <li>
-                    <Link to="/character">Personagens</Link>
+                    <Link className="link" to="/character"><Btn text='Personagens'/></Link>
                     </li>
                     <li>
-                    <Link to="/locations">Planetas</Link>
+                    <Link className="link" to="/location"><Btn text='Localização'/></Link>
                     </li>
                     <li>
-                    <Link to="/about">Sobre</Link>
+                    <Link className="link" to="/about"><Btn text='Sobre'/></Link>
                     </li>
                 </ul>
             </nav>
         </header>
+        <Hr></Hr>
+        <HrPurple></HrPurple>
+        </>
     )
 }
