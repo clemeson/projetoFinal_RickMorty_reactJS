66986af579048e32a6ea86485b6f336106f23040
Luis Henrique <LuisHenSilva@Outlook.com>
2021-11-04 18:17:04 -0300

    home

--- src/views/home/index.js
+++ src/views/home/index.js
@@ -1,14 +1,27 @@
 import React from "react"
 import './style.css'
+import imagem  from './portall.gif';
+import rick from './and.gif'
+import core from './core.gif';
+
 
 function Home(){
 
 
     return (
 
-        <>
-        <h1>Pagina home</h1>
-        </>
+        <div className="pai">
+        
+                    <h1 className="nome1">R I C K </h1>
+                    <h2 className="nome2"> A N D </h2>
+                    <h3 className="nome3"> M O R T Y</h3>
+                
+            <img className="img" src={imagem} alt=""/> 
+            <img className="img2" src={rick} alt=""/>
+            <img className="img3" src={core} alt=""/>
+             
+        </div>
+    
     )
 }
 
