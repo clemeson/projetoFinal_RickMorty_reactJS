57494dbab708ebad110cba2bf9f9ade6328ac94d
Bruno Rodrigo <brunnogost2017@gmail.com>
2021-11-11 13:07:24 -0300

    resolvendo falhas h1 e h2

--- src/views/home/index.js
+++ src/views/home/index.js
@@ -19,7 +19,7 @@ export default function Home() {
             </section>
             <section className='sections'>
                 <div id='text1'>
-                    <h1>Pronto para aventurar-se no multiverso Rick and Morty?</h1>
+                    <h1 id='h1'>Pronto para aventurar-se no multiverso Rick and Morty?</h1>
                     <p id='p'>Navegue num mar de curiosidades da nossa comunidade de fãs da melhor série animada do mundo!</p> 
                 </div>
                 <div id='portal-div'>
@@ -32,7 +32,7 @@ export default function Home() {
                     <img className='heads' src={mortyhead}></img>
                 </div>
                 <div id='text2'>
-                    <h2>Vamos viajar num mundo diferente, perscrutar dimensões e galáxias, conhecer personagens e por onde andam a nossa dupla de viajantes intergalácticos preferida.</h2>
+                    <h2 id='h2'>Vamos viajar num mundo diferente, perscrutar dimensões e galáxias, conhecer personagens e por onde andam a nossa dupla de viajantes intergalácticos preferida.</h2>
                     <Link id="link" to="/character"> Vamos Começar?</Link>
                     <p id='sms-link'>Cique aqui para ser redirecionado à página de personagens.</p>
                 </div>
