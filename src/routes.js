c6b0e4636a314d4e2549092dc8299bef85e8587f
clemeson <clemeson4lves@gmail.com>
2021-11-14 20:11:02 -0300

    estruturando login

--- src/routes.js
+++ src/routes.js
@@ -1,20 +1,28 @@
-import React from "react";
-import { Switch, Route} from "react-router-dom";
+import React, {useState}from "react";
+import { Switch, Route, Redirect} from "react-router-dom";
 import Personagens from "./views/characters";
 import Sobre from "./views/about";
 import GalleryComponent from "./views/about/Gallery";
 import Location from "./views/location";
 import Home from "./views/home";
 import PageNotFound from './views/notfound/index'
+import Forum from './views/forum/index'
 
  function Routes(){
 
+      const [isLogged, setIsLogged]  = useState('')
 
             return(
                          <Switch>
 
                               <Route path="/" exact component={Home} />
                               <Route path="/home" exact component={Home} />
+
+                              <Route path="/forum" >
+                              {isLogged ? <Forum /> : <Redirect to="/login"/>}
+                               </Route>
+                              
+                              
 
 
                               <Route path="/character"  exact component={Personagens} />
---
+++ src/views/forum/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './styles.css/style.css'
+import './styles.css/responsive.css'
+
+
+function Forum(){
+
+
+
+    return(
+
+        <>
+        <h1>forum</h1>
+        </>
+    )
+}
+
+export default Forum
