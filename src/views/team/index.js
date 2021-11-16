049443b937a8e145a054cada36d9a5c24ba47deb
Marcos2k02 <marquinhosmaciel44@gmail.com>
2021-11-16 16:17:53 -0300

    subindo alterações na sobre e tela de desenvolvedore

--- src/views/team/index.js
+++ src/views/team/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./index.css";
 
 
 
@@ -6,35 +7,40 @@ function QuemSomos(){
     return(
         <>
             <main id="mainQuemSomos">
-                <div>
-                    <img id="imagem1" src="./bruno.jpg" alt=""></img>
-                    <p>dsfdfdsdsf</p>
-                    <p>dfdsfdsfds</p>
-                    <p>dsfdsfds</p>
+                <div id="divGeral">
+                    <img class="imagem" src="./bruno.jpg" alt=""></img>
+                    <h3>Bruno Rodrigo</h3>
+                    <p class="info">19 Anos</p>
+                    <p class="info">brunorodrigoteixeira2021@gmail.com</p>
+                    <p></p>
                 </div>
-                <div>
-                    <img id="imagem2" src="./clemersson.jpg" alt=""></img>
-                    <p>dsfdsfds</p>
-                    <p>dfdsfdsfsdfs</p>
-                    <p>dsfdsfdsfsdf</p>
+                <div id="divGeral">
+                    <img class="imagem" src="./clemersson.jpg" alt=""></img>
+                    <h3>Clemersson Alves</h3>
+                    <p class="info">24 Anos</p>
+                    <p class="info">clemeson4lves@gmail.com</p>
+                    <p class="info"></p>
                 </div>
-                <div>
-                    <img id="imagem3" src="./eduarda.jpg" alt=""></img>
-                    <p>sdfdsfdsfsdf</p>
-                    <p>sdfdsfdfdsf</p>
-                    <p>dsfdsfdsfdsf</p>
+                <div id="divGeral">
+                    <img class="imagem" src="./eduarda.jpg" alt=""></img>
+                    <h3>Maria Eduarda</h3>
+                    <p class="info">22 Anos</p>
+                    <p class="info">mariaeduarda.fs21@outlook.com</p>
+                    <p class="info"></p>
                 </div>
-                <div>
-                    <img id="imagem4" src="./marcos.png" alt=""></img>
-                    <p>dsfdsfdsfdsf</p>
-                    <p>dsfdsfsfdsf</p>
-                    <p>sdfdsfdsfds</p>
+                <div id="divGeral">
+                    <img class="imagem" src="./marcos.png" alt=""></img>
+                    <h3>Marcos Antônio</h3>
+                    <p class="info">19 Anos</p>
+                    <p class="info">marquinhosmaciel44@gmail.com</p>
+                    <p class="info"></p>
                 </div>
-                <div>
-                    <img id="imagem5" src="./luiz.jpg" alt=""></img>
-                    <p>dsfdsfsfdsf</p>
-                    <p>sdfdsfdsfsdf</p>
-                    <p>sdfdsfdsfds</p>
+                <div id="divGeral">
+                    <img class="imagem" src="./luiz.jpg" alt=""></img>
+                    <h3>Luis Henrique</h3>
+                    <p class="info">23 Anos</p>
+                    <p class="info">luishenrique978754@gmail.com</p>
+                    <p class="info"></p>
                 </div>
             </main>
         </>
