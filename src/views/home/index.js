3af37e7e77c93ee584f5c2aa08ea8c72af260c11
Bruno Rodrigo <brunnogost2017@gmail.com>
2021-11-16 11:49:39 -0300

    galeria npm

--- src/views/home/index.js
+++ src/views/home/index.js
@@ -48,93 +48,82 @@ export default function Home() {
             <div id="containerHome">
                 
                 <section id='sec-title'>
-                <div className="container-topo">
-                    <div className="container_text">
-                    <img className="img_tabela" src={frame}/>
-                    <button className="button_started" onClick={setModal}>Get Started</button>
-
-                    </div>
-                    <div className="img_container">
-                        <img className="img_black" src={img_black} />
-                    </div>
+                    <div className="container-topo">
+                        <div className="container_text">
+                            <img className="img_tabela" src={frame}/>
+                            <button className="button_started" onClick={setModal}>Get Started</button>
+                        </div>
+                        <div className="img_container">
+                            <img className="img_black" src={img_black} />
+                        </div>
                     </div>
                     <a href="#sec-title1" className="container-down" ><img class="icon-down" src={iconDown}/></a>
                 </section>
 
 
                 <section id='sec-title'>
-                <div className="container-topo">
-                    <div className="container_text">
-                    <h1 className="inicio "> Pronto para aventurar-se no multiverso Rick and Morty? </h1>
-                    
-
-                    </div>
-                    <div className="img_container">
-                   
-
-                    </div>
-
-                        <img className="img_black1" src={portal} />
+                    <div className="container-topo">
+                        <div className="container_text">
+                            <h1 className="inicio "> Pronto para aventurar-se no multiverso Rick and Morty? </h1>
+                        </div>
+                        
+                        <div className="img_container">
+                            <img className="img_black1" src={portal} />
+                        </div>
                     </div>
                   
                     <a href="#sec-title1" className="container-down" ><img class="icon-down" src={iconDown}/></a>
                 </section>
                 <section id='sec-title'>
-                <div className="container-topo">
-                    <div className="container_text">
-                    <h1 className="perso"> Vamos viajar num mundo diferente, perscrutar dimensões e galáxias, conhecer personagens e por onde andam a nossa dupla de viajantes intergalácticos preferida</h1>
+                    <div className="container-topo">
+                        <div className="container_text">
+                            <h1 className="perso"> Vamos viajar num mundo diferente, perscrutar dimensões e galáxias, conhecer personagens e por onde andam a nossa dupla de viajantes intergalácticos preferida</h1>
+                        </div>
 
-                    </div>
-                    <div className="img_container7">
-                    <Link id="link" to="/character"> Vamos Começar?</Link>
-                    <p id='sms-link'>Cique aqui para ser redirecionado à página de personagens.</p>
-                    </div>
+                        <div className="img_container7">
+                            <Link id="link" to="/character"> Vamos Começar?</Link>
+                            <p id='sms-link'>Cique aqui para ser redirecionado à página de personagens.</p>
+                        </div>
                     </div>
                     <a href="#sec-title1" className="container-down" ><img class="icon-down" src={iconDown}/></a>
                 </section>
 
                 <section id='sec-title'>
-                <div className="container-topo">
-                    <div className="container_text">
-                    <h1 className="juntos"> Navegue num mar de curiosidades da nossa comunidade de fãs da melhor série animada do mundo! </h1>
-                    
-                    
-                    </div>
-                    <div className="img_container">
-                        <img className="img_black3" src={mortyhead} />
-                        <img className="img_black7" src={rickhead } />
-                    </div>
+                    <div className="container-topo">
+                        <div className="container_text">
+                            <h1 className="juntos"> Navegue num mar de curiosidades da nossa comunidade de fãs da melhor série animada do mundo! </h1>
+                        </div>
+                        <div className="img_container">
+                            <img className="img_black3" src={mortyhead} />
+                            <img className="img_black7" src={rickhead } />
+                        </div>
                     </div>
                     <a href="#sec-title1" className="container-down" ><img class="icon-down" src={iconDown}/></a>
                 </section>
 
                 <section id='sec-title'>
-                <div className="container-topo">
-                    <div className="container_text">
-                    <h1  className="mobile"> Experimente também na versão Mobile!</h1>
-                    
-
-                    </div>
-                    <div className="img_container">
-                        <img className="img_black4" src={gifcell } />
-                    </div>
+                    <div className="container-topo">
+                        <div className="container_text">
+                            <h1  className="mobile"> Experimente também na versão Mobile!</h1>
+                        </div>
+                        <div className="img_container">
+                            <img className="img_black4" src={gifcell } />
+                        </div>
                     </div>
                     <a href="#sec-title1" className="container-down" ><img class="icon-down" src={iconDown}/></a>
                 </section>
                 <section id='sec-title'>
-                <div className="container-topo">
-                    <div className="container_text">
-                   <h1 className="bora"> Ninguém existe por um propósito. Ninguém pertence a lugar nenhum. Todo mundo vai morrer. Vem ver TV </h1>
-
-                    </div>
-                    <div className="img_container">
-                    <a href= " https://www.netflix.com/br/ " target="_blank"> <img className="img_black5" src={net} /> </a> 
-                     <a  href="https://play.hbomax.com/page/urn:hbo:page:home"  target="blank"> <img className="img_black8" src={hbo} /> </a>
-                    </div>
+                    <div className="container-topo">
+                        <div className="container_text">
+                            <h1 className="bora"> Ninguém existe por um propósito. Ninguém pertence a lugar nenhum. Todo mundo vai morrer. Vem ver TV </h1>
+                        </div>
+                        <div className="img_container">
+                            <a href= " https://www.netflix.com/br/ " target="_blank"> <img className="img_black5" src={net} /> </a> 
+                            <a  href="https://play.hbomax.com/page/urn:hbo:page:home"  target="blank"> <img className="img_black8" src={hbo} /> </a>
+                        </div>
                     </div>
                     <a href="#sec-title1" className="container-down" ><img class="icon-down" src={iconDown}/></a>
                 </section>
-
             </div>
         </div>
     )
